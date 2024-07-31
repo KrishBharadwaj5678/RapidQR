2aeda51e3845b6dadf5ae86728f6235477d62499
Krish Bharadwaj <167067349+KrishBharadwaj5678@users.noreply.github.com>
2024-08-01 00:51:23 +0530

    Update index.js

--- QRCode/index.js
+++ QRCode/index.js
@@ -1,44 +1,51 @@
-
+let t=gsap.timeline()
 let qr_textinput=document.querySelector("input.qr_textinput");
-
 let generate_qr=document.querySelector("button.generate");
-
 let qr_download=document.querySelector("button.download_qr");
-
 let download=document.querySelector("a.download");
-
 let qr_img=document.querySelector("img.qr");
-
 let downarrow1=document.querySelector("i.downarrow1");
-
 let uparrow1=document.querySelector("i.uparrow1");
-
 let downarrow2=document.querySelector("i.downarrow2");
-
 let uparrow2=document.querySelector("i.uparrow2");
-
 let downarrow3=document.querySelector("i.downarrow3");
-
 let uparrow3=document.querySelector("i.uparrow3");
-
 let explain1=document.querySelector("p.explain1");
-
 let explain2=document.querySelector("p.explain2");
-
 let explain3=document.querySelector("p.explain3");
-    
+
+t.from("img.qr",{
+    opacity:0,
+    x:100
+})
+
+downarrow1.addEventListener("click",()=>{
+    t.from("p.explain1",{
+        x:25,
+        opacity:0
+    })
+})
+
+downarrow2.addEventListener("click",()=>{
+    t.from("p.explain2",{
+        x:25,
+        opacity:0
+    })
+})
+
+downarrow3.addEventListener("click",()=>{
+    t.from("p.explain3",{
+        x:25,
+        opacity:0
+    })
+})
+
 generate_qr.addEventListener("click",()=>{
-
     let data=qr_textinput.value;
-
     if(data.length>=1){
-        
         let qr_url=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${data}`;
-
         qr_img.src=qr_url;
-
     }
-
     else{
         qr_img.src="qr.jpeg";
     }
@@ -81,7 +88,6 @@ download();
 })
 
 // Arrow Functionality
-
 document.body.addEventListener("click",(e)=>{
     let data=e.target.className;
     if(data.includes("downarrow1")){
@@ -102,11 +108,9 @@ document.body.addEventListener("click",(e)=>{
     else if(data.includes("downarrow3")){
         downarrows(explain3,downarrow3,uparrow3)
     }
-    
 })
 
 // Down Arrow Functionality
-
 function downarrows(explain,downarrow,uparrow){
         explain.style.display="block";
         downarrow.style.display="none";
@@ -114,10 +118,8 @@ function downarrows(explain,downarrow,uparrow){
 }
 
 // Up Arrow Functionality
-
 function uparrows(explain,downarrow,uparrow){
         explain.style.display="none";
         downarrow.style.display="block";
         uparrow.style.display="none";
 }
-
